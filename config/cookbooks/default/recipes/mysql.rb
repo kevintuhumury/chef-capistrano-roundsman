49042efc08060017640d66f0f919969902f10779
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-03-19 08:14:13 +0100

    Moved the MySQL recipe to the default cookbook. There is a workaround in these files to set the server_root_password and password attributes. These attributes can't be set in the config/deploy/<stage>.rb file (not sure why), so the values are assigned to a temporary variable and in the mysql recipe they are assigned to the original attributes.

--- config/cookbooks/default/recipes/mysql.rb
+++ config/cookbooks/default/recipes/mysql.rb
@@ -2,6 +2,12 @@ package 'mysql-server'
 package 'mysql-client'
 package 'libmysqlclient-dev'
 
+# NOTE: for some reason the server_root_password and password attributes can't
+# be set directly in the config/deploy/<stage>.rb file. So, this is a workaround.
+
+node.default[:mysql][:server_root_password] = node[:mysql][:root_passwd]
+node.default[:mysql][:password]             = node[:mysql][:passwd]
+
 execute "set mysql root password" do
   command "mysqladmin -u root password '#{node[:mysql][:server_root_password]}'"
   only_if "mysql -u root -e 'show databases' > /dev/null"
