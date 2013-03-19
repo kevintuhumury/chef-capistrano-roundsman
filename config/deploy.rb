6da0a332f9f94e1706b81888609448e0f6b0e491
Kevin Tuhumury <kevin.tuhumury@gmail.com>
2013-03-19 08:17:43 +0100

    Updated the main deploy file.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,47 +1,51 @@
-set :stages,              %w(vagrant production)
-set :cookbooks_directory, ["config/cookbooks", "vendor/cookbooks"]
+load "config/recipes/base"
+load "config/recipes/formatting"
+load "config/recipes/helpers"
 
-set :use_sudo,            false
-set :keep_releases,       10
+server "<server>",          :web, :app, :db, primary: true
 
-set :default_run_options, :pty => true
-set :ssh_options,         :forward_agent => true
+set :stages,                %w(apache-rails nginx-rails nginx-middleman vagrant)
+set :cookbooks_directory,   %w(config/cookbooks vendor/cookbooks)
+set :application_directory, defer { application }
+set :deploy_to,             defer { "/var/www/#{application_directory}" }
 
-before "bundle:install", "app:update_current_release"
+set :user,                  "<username>"
+set :password,              "<password>"
+set :copy_exclude,          [".git", ".gitignore", ".DS_Store", ".rvmrc"]
 
-after "deploy:finalize_update", "app:symlink"
-after "deploy:update_code",     "app:remove_rvmrc"
-after "deploy",                 "deploy:cleanup"
+set :rails_env,             "production"
+set :ruby_version,          "1.9.3-p392"
+set :chef_version,          "11.2.0"
+
+set :use_sudo,              false
+set :default_run_options,   pty: true
+set :ssh_options,           forward_agent: true
+set :keep_releases,         10
+
+before "bundle:install",    "app:update_current_release"
+
+after "deploy:update_code", "deploy:cleanup"
 
 namespace :deploy do
   [:start, :stop].each do |task|
-    desc "#{task} task is a no-op with passenger"
-    task task, :roles => :app do ; end
+    desc "#{task} task isn't needed with passenger"
+    task task, roles: :app do ; end
   end
 
   desc "restart passenger"
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "touch #{File.join(current_path,'tmp','restart.txt')}"
+  task :restart, roles: :app, except: { no_release: true } do
+    run "touch #{File.join(current_path, 'tmp/restart.txt')}"
   end
 end
 
 namespace :app do
-  desc "symbolic link to the shared assets"
-  task :symlink, :roles => [:app] do
-    run "ln -sf #{shared_path}/database.yml #{release_path}/config/database.yml"
+  desc "builds a middleman app"
+  task :middleman, roles: :app do
+    build_middleman
   end
 
-  desc "update the release path, since it's using the previous one"
-  task :update_current_release do
+  desc "update the current_release path"
+  task :update_current_release, roles: :app do
     set :current_release, release_path
   end
-
-  desc "remove rvmrc"
-  task :remove_rvmrc, :roles => [:app] do
-    run "rm #{release_path}/.rvmrc" if file_exists? "#{release_path}/.rvmrc"
-  end
-end
-
-def file_exists?(path)
-  "true" ==  capture("if [ -e #{path} ]; then echo 'true'; fi").strip
 end
